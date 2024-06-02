62fd378914c3ea7df623c9a5dc110fc8854bb888
julian-hart <julianja.hart@gmail.com>
2024-06-01 22:02:42 -0400

    Created file structure and finished nav component for the layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,29 @@
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
+import { children } from "react";
+
+export const metadata = {
+  title: "Promptpedia",
+  description: "Discover and Create Helpful AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
